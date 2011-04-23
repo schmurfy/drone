a5dbda5b2aebe239a5f1f31757763f0fad1fa442
Julien Ammous <j.ammous@gmail.com>
2011-04-23 21:42:12 +0200

    added utils to the Drone module

--- lib/drone/utils/exponentially_decaying_sample.rb
+++ lib/drone/utils/exponentially_decaying_sample.rb
@@ -1,71 +1,72 @@
-
-class ExponentiallyDecayingSample
-  # 1 hour in ms
-  RESCALE_THRESHOLD = (1 * 60 * 60 * 1000).freeze
+module Drone
+  class ExponentiallyDecayingSample
+    # 1 hour in ms
+    RESCALE_THRESHOLD = (1 * 60 * 60 * 1000).freeze
   
-  def initialize(reservoir_size, alpha)
-    @values = {}
-    @alpha = alpha
-    @reservoir_size = reservoir_size
-    clear()
-  end
+    def initialize(reservoir_size, alpha)
+      @values = {}
+      @alpha = alpha
+      @reservoir_size = reservoir_size
+      clear()
+    end
   
-  def clear
-    @values.clear()
-    @count = 0
-    @start_time = Time.now
-    @next_scale_time = current_time() + RESCALE_THRESHOLD
-  end
+    def clear
+      @values.clear()
+      @count = 0
+      @start_time = Time.now
+      @next_scale_time = current_time() + RESCALE_THRESHOLD
+    end
   
-  def size
-    (@values.size < @count) ? @values.size : @count
-  end
+    def size
+      (@values.size < @count) ? @values.size : @count
+    end
   
   
-  def update(val, time = Time.now)
-    priority = weight(time - @start_time) / rand()
-    @count += 1
-    if @count <= @reservoir_size
-      @values[priority] = val
-    else
-      first = @values.keys.min
-      if first < priority
+    def update(val, time = Time.now)
+      priority = weight(time - @start_time) / rand()
+      @count += 1
+      if @count <= @reservoir_size
         @values[priority] = val
-        while @values.delete(first) == nil
-          first = @values.keys.min
+      else
+        first = @values.keys.min
+        if first < priority
+          @values[priority] = val
+          while @values.delete(first) == nil
+            first = @values.keys.min
+          end
         end
       end
+    
+      now = current_time()
+      if now >= @next_scale_time
+        rescale(now)
+      end
     end
-    
-    now = current_time()
-    if now >= @next_scale_time
-      rescale(now)
+  
+    def values
+      @values.values
     end
-  end
   
-  def values
-    @values.values
-  end
-  
-  def rescale(now)
-    @next_scale_time = current_time() + RESCALE_THRESHOLD
-    old_start = @start_time
-    @start_time = Time.now
+    def rescale(now)
+      @next_scale_time = current_time() + RESCALE_THRESHOLD
+      old_start = @start_time
+      @start_time = Time.now
     
-    @values = Hash[ @values.map{ |k,v|
-        [k * Math.exp(-@alpha * (@start_time - old_start)), v]
-      }]
+      @values = Hash[ @values.map{ |k,v|
+          [k * Math.exp(-@alpha * (@start_time - old_start)), v]
+        }]
     
+    end
+  
+  private
+  
+    def current_time
+      Time.now.to_f * 1000
+    end
+  
+    def weight(n)
+      Math.exp(@alpha * n)
+    end
+  
   end
-  
-private
-  
-  def current_time
-    Time.now.to_f * 1000
-  end
-  
-  def weight(n)
-    Math.exp(@alpha * n)
-  end
-  
 end
--- lib/drone/utils/uniform_sample.rb
+++ lib/drone/utils/uniform_sample.rb
@@ -1,37 +1,39 @@
-class UniformSample
+module Drone
+  class UniformSample
   
-  def initialize(size)
-    @values = Array.new(size)
-    clear()
-  end
-  
-  def clear
-    @values.size.times do |n|
-      @values[n] = 0
+    def initialize(size)
+      @values = Array.new(size)
+      clear()
     end
+  
+    def clear
+      @values.size.times do |n|
+        @values[n] = 0
+      end
     
-    @count = 0
-  end
+      @count = 0
+    end
   
-  def size
-    (@count > @values.size) ? @values.size : @count
-  end
+    def size
+      (@count > @values.size) ? @values.size : @count
+    end
   
-  def update(val)
-    @count += 1
-    if @count <= @values.size
-      @values[@count - 1] = val
-    else
-      r = rand(2**64 - 1) % @count
-      if r < @values.size
-        @values[r] = val
+    def update(val)
+      @count += 1
+      if @count <= @values.size
+        @values[@count - 1] = val
+      else
+        r = rand(2**64 - 1) % @count
+        if r < @values.size
+          @values[r] = val
+        end
       end
     end
-  end
   
-  def values
-    # only return @count elements
-    @values[0,@count]
-  end
+    def values
+      # only return @count elements
+      @values[0,@count]
+    end
   
+  end
 end
